b2f1dae0d9f8e4d2e1af0d2398ab8f614eaad20c
Aiden-Changyeol-Oh <changyeol.oh@stonybook.edu>
2026-04-16 16:01:18 +0900

    Remove temporary check_data.py

--- voc_pipeline/check_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-blog = pd.read_csv("data/raw/naver_blog_hns.csv")
-cafe = pd.read_csv("data/raw/naver_cafe_hns.csv")
-yt = pd.read_csv("data/raw/youtube_comments_hns.csv")
-
-print(blog[["title", "description"]].head(10).to_string())
-print()
-print(yt[["comment"]].head(20).to_string())
